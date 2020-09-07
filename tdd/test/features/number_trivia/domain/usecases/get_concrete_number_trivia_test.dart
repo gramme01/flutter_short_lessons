7c3612bd876646d91f964e91633fe32744f827c4
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-09-07 22:30:29 +0100

    First tdd

--- tdd/test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ tdd/test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -1,8 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:tdd/features/number_trivia/domain/entities/number_trivia.dart';
 
+import 'package:tdd/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:tdd/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 
@@ -29,5 +29,6 @@ void main() {
     //assert
     expect(result, Right(tNumberTrivia));
     verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+    verifyNoMoreInteractions(mockNumberTriviaRepository);
   });
 }
