97ba51747af4bd79101a3070c365beb35618681d
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-09-09 02:07:57 +0100

    Network  Info

---
+++ tdd/test/core/network/network_info_test.dart
@@ -0,0 +1,28 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:tdd/core/network/network_info.dart';
+
+void main() {
+  NetworkInfoImpl networkInfo;
+  MockDataConnectionChecker mockDataConnectionChecker;
+
+  setUp(() {
+    mockDataConnectionChecker = MockDataConnectionChecker();
+    networkInfo = NetworkInfoImpl(mockDataConnectionChecker);
+  });
+
+  group('isConnected', () {
+    test('should forward the call to DataConnectionChecker.hasConnection',
+        () async {
+      final tHasConnectionFuture = Future.value(true);
+      when(mockDataConnectionChecker.hasConnection)
+          .thenAnswer((_) => tHasConnectionFuture);
+      final result = networkInfo.isConnected;
+      verify(mockDataConnectionChecker.hasConnection);
+      expect(result, tHasConnectionFuture);
+    });
+  });
+}
+
+class MockDataConnectionChecker extends Mock implements DataConnectionChecker {}
--- tdd/test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
+++ tdd/test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
@@ -1,21 +1,15 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+
 import 'package:tdd/core/error/exceptions.dart';
 import 'package:tdd/core/error/failures.dart';
-import 'package:tdd/core/platform/network_info.dart';
+import 'package:tdd/core/network/network_info.dart';
 import 'package:tdd/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:tdd/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:tdd/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:tdd/features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
 import 'package:tdd/features/number_trivia/domain/entities/number_trivia.dart';
-
-class MockRemoteDataSource extends Mock
-    implements NumberTriviaRemoteDataSource {}
-
-class MockLocalDataSource extends Mock implements NumberTriviaLocalDataSource {}
-
-class MockNetworkInfo extends Mock implements NetworkInfo {}
 
 void main() {
   NumberTriviaRepositoryImpl repository;
@@ -189,3 +183,10 @@ void main() {
     });
   });
 }
+
+class MockLocalDataSource extends Mock implements NumberTriviaLocalDataSource {}
+
+class MockNetworkInfo extends Mock implements NetworkInfo {}
+
+class MockRemoteDataSource extends Mock
+    implements NumberTriviaRemoteDataSource {}
