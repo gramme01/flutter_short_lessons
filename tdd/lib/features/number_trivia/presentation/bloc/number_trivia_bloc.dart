28f19f2c838ad82a53bc72abb4c9434b0cf3502b
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-09-12 06:42:28 +0100

    Bloc Implementation 2

--- tdd/lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ tdd/lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+import 'package:tdd/core/error/failures.dart';
+import 'package:tdd/core/usecases/usecase.dart';
 import 'package:tdd/core/util/input_converter.dart';
 import 'package:tdd/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 import 'package:tdd/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
@@ -46,9 +49,34 @@ class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
         yield Error(message: INVALID_INPUT_FAILURE_MESSAGE);
       }, (integer) async* {
         yield Loading();
-        // final failureOrTrivia =
-        //     await getConcreteNumberTrivia(Params(number: integer));
+        final failureOrTrivia =
+            await getConcreteNumberTrivia(Params(number: integer));
+        yield* _eitherErrorOrLoadedState(failureOrTrivia);
       });
+    } else if (event is GetTriviaForRandomNumber) {
+      yield Loading();
+      final failureOrTrivia = await getRandomNumberTrivia(NoParams());
+      yield* _eitherErrorOrLoadedState(failureOrTrivia);
+    }
+  }
+
+  Stream<NumberTriviaState> _eitherErrorOrLoadedState(
+      Either<Failure, NumberTrivia> either) async* {
+    yield either.fold(
+      (failure) => Error(message: _mapFailureToMessage(failure)),
+      (trivia) => Loaded(trivia: trivia),
+    );
+  }
+
+//this can be done with extension method C12 19:52
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return SERVER_FAILURE_MESSAGE;
+      case CacheFailure:
+        return CACHE_FAILURE_MESSAGE;
+      default:
+        return 'Unexpected error';
     }
   }
 }
