31795c285645eb3c380aeae5babfe4425b6dd264
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-09-14 22:51:29 +0100

    Theme bloc

--- dynamic_theming_bloc/lib/ui/global/theme/bloc/theme_bloc.dart
+++ dynamic_theming_bloc/lib/ui/global/theme/bloc/theme_bloc.dart
@@ -1,20 +1,24 @@
 import 'dart:async';
-import 'package:meta/meta.dart';
 
 import 'package:bloc/bloc.dart';
-import 'package:dynamic_theming_bloc/ui/global/theme/app_themes.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+import '../app_themes.dart';
 
 part 'theme_event.dart';
 part 'theme_state.dart';
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
-  ThemeBloc() : super(ThemeInitial());
+  ThemeBloc() : super(ThemeState(themeData: appThemeData[AppTheme.GreenLight]));
 
   @override
   Stream<ThemeState> mapEventToState(
     ThemeEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is ThemeChanged) {
+      yield ThemeState(themeData: appThemeData[event.theme]);
+    }
   }
 }
