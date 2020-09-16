91aa782ada547f86ca2e06daa0a60725f14eb7ec
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-09-16 08:09:44 +0100

    Refactor

--- bloc_library/lib/main.dart
+++ bloc_library/lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:bloc_library/data/weather_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'bloc/weather_bloc.dart';
 import 'pages/weather_search_page.dart';
 
 void main() => runApp(MyApp());
@@ -9,7 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Weather App',
-      home: WeatherSearchPage(),
+      home: BlocProvider(
+        create: (context) => WeatherBloc(FakeWeatherRepository()),
+        child: WeatherSearchPage(),
+      ),
     );
   }
 }
